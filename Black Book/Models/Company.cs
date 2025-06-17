82c1169815aa963158c783629760f7795869f00a
incorrigo syx <westid@incorrigo.io>
2025-06-17 20:59:01 +0100

    Model structure completion

--- Black Book/Models/Company.cs
+++ Black Book/Models/Company.cs
@@ -1,7 +1,10 @@
-﻿namespace BlackBook.Models;
+﻿using System;
+
+namespace BlackBook.Models;
 
 public class Company {
-    public string Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; }
     public string Address { get; set; }
+    public string Description { get; set; }
 }
--- Black Book/Models/Interaction.cs
+++ Black Book/Models/Interaction.cs
@@ -1,10 +1,30 @@
-﻿namespace BlackBook.Models;
+﻿using System;
+
+namespace BlackBook.Models;
 
 public class Interaction {
-    public string Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public string PersonId { get; set; }
     public string CompanyId { get; set; }
-    public DateTime Timestamp { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public InteractionDirection Direction { get; set; }
-    public string Details { get; set; }
+    public InteractionType Type { get; set; }
+    public string Notes { get; set; }
+    public string SituationId { get; set; } // clearly linked to Situation
+}
+
+public enum InteractionDirection {
+    Incoming,
+    Outgoing
+}
+
+public enum InteractionType {
+    Email,
+    PhoneCall,
+    TextMessage,
+    InPersonMeeting,
+    VideoConference,
+    PostalMail,
+    SocialMedia,
+    Other
 }
