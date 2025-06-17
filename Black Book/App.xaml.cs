56470fd4f9d3d7b3338d8c76dc026e686e26d164
incorrigo syx <westid@incorrigo.io>
2025-06-17 06:23:45 +0100

    Add project files.

---
+++ Black Book/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Black_Book
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
