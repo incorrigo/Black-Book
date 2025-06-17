d03e40ac9da8c85babae02fe3e2d08e5c61596a1
incorrigo syx <westid@incorrigo.io>
2025-06-17 20:59:01 +0100

    Model structure completion

--- Black Book/Models/Person.cs
+++ Black Book/Models/Person.cs
@@ -1,8 +1,19 @@
-﻿namespace BlackBook.Models;
+﻿using System;
+
+namespace BlackBook.Models;
 
 public class Person {
-    public string Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Phone { get; set; }
+    public string PhoneNumber { get; set; }
+    public string CompanyId { get; set; }
+    public RelationshipType Relationship { get; set; } = RelationshipType.Unknown;
+}
+
+public enum RelationshipType {
+    Unknown,
+    Friendly,
+    Neutral,
+    Adversarial
 }
---
+++ Black Book/Models/Situation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlackBook.Models;
+
+public class Situation {
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public SituationStatus Status { get; set; } = SituationStatus.AdHoc;
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime? Closed { get; set; }
+}
+
+public enum SituationStatus {
+    AdHoc,
+    New,
+    Ongoing,
+    DoneWith
+}
