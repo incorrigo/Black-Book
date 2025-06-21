a2ab551a09647691ed6554a9e6baee6879c960ec
incorrigo syx <westid@incorrigo.io>
2025-06-21 03:21:15 +0100

    removed theme pointlessness and back to grown up programming

--- Black Book/App.xaml.cs
+++ Black Book/App.xaml.cs
@@ -1,50 +1,6 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace BlackBook;
 
 public partial class App : Application {
-
-    private void App_Startup (object sender, StartupEventArgs e) {
-        var main = new MainWindow();
-        main.Show();
-    }
-
-    protected override void OnStartup (StartupEventArgs e) {
-        // 🔥 CRITICAL: Force software rendering to allow transparency
-        System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
-
-        base.OnStartup(e);
-
-        EventManager.RegisterClassHandler(typeof(Grid),
-            UIElement.MouseLeftButtonDownEvent,
-            new MouseButtonEventHandler(SyxBarDrag));
-
-        EventManager.RegisterClassHandler(typeof(Window),
-            Button.ClickEvent,
-            new RoutedEventHandler(SyxChrome_CloseBtn_Click));
-    }
-
-
-    private static void SyxBarDrag (object sender, MouseButtonEventArgs e) {
-        if (sender is Grid g && g.Name == "SyxBar") {
-            Window w = Window.GetWindow(g);
-            if (e.ClickCount == 2) {
-                w.WindowState = w.WindowState == WindowState.Normal
-                    ? WindowState.Maximized
-                    : WindowState.Normal;
-            }
-            else {
-                w.DragMove();
-            }
-        }
-    }
-
-    private static void SyxChrome_CloseBtn_Click (object sender, RoutedEventArgs e) {
-        if (e.OriginalSource is Button btn && btn.Content?.ToString() == "✕") {
-            Window w = Window.GetWindow(btn);
-            w?.Close();
-        }
-    }
 }
--- Black Book/MainWindow.xaml.cs
+++ Black Book/MainWindow.xaml.cs
@@ -1,99 +1,21 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
+﻿using System.Windows;
 
-namespace BlackBook;         // <‑‑ must match x:Class minus .MainWindow
+namespace BlackBook;
 
 public partial class MainWindow : Window {
-    private readonly string keyDir =
-        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Keys");
-
     public MainWindow () {
-        this.WindowStyle = WindowStyle.None;
-        this.AllowsTransparency = true;
-        this.ResizeMode = ResizeMode.NoResize;
-        this.Background = Brushes.Transparent;
-
-
-        WindowInteropHelper helper = new(this);
-        int style = NativeMethods.GetWindowLong(helper.Handle, -16);
-        style &= ~0x00C00000; // WS_CAPTION
-        NativeMethods.SetWindowLong(helper.Handle, -16, style);
-
-
         InitializeComponent();
     }
 
-    internal static class NativeMethods {
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        internal static extern int GetWindowLong (IntPtr hWnd, int nIndex);
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        internal static extern int SetWindowLong (IntPtr hWnd, int nIndex, int dwNewLong);
-    }
-
-
-    private void SyxBar_MouseLeftButtonDown (object sender, MouseButtonEventArgs e) {
-        if (e.ClickCount == 2) {
-            // Optionally maximize/restore, if you want (not needed if NoResize)
-            // WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
-        }
-        else if (e.LeftButton == MouseButtonState.Pressed) {
-            DragMove();
-            e.Handled = true;
-        }
-    }
-
-    // Custom close button handler
-    private void CloseBtn_Click (object sender, RoutedEventArgs e) {
-        this.Close();
-    }
-
-
-    /* ---------- UI logic ---------- */
-
-    private void RefreshButtons () {
-        Directory.CreateDirectory(keyDir);
-        var hasKey = Directory.EnumerateFiles(keyDir, "*.pfx").Any();
-        CreateKeyBtn.IsEnabled = true;
-        OpenBookBtn.IsEnabled = hasKey;
-    }
-
-    /* ---------- button handlers ---------- */
-
     private void CreateKey_Click (object sender, RoutedEventArgs e) {
-        new Views.CertificateSetupWindow { Owner = this }.ShowDialog();
-        RefreshButtons();
+        new Views.CertificateSetupWindow().ShowDialog();
     }
 
     private void OpenBook_Click (object sender, RoutedEventArgs e) {
-        var keys = Directory.EnumerateFiles(keyDir, "*.pfx").ToList();
-        if (keys.Count == 0) return;
-
-        string chosen = keys.Count == 1 ? keys[0] : PromptForKey(keys);
-        if (chosen == null) return;
-
-        // TODO: password prompt + cert load
-        new Views.SituationManager { Owner = this }.Show();
-        Hide();
+        new Views.SituationManager().ShowDialog();
     }
 
-    private void Quit_Click (object sender, RoutedEventArgs e) => Close();
-
-    /* ---------- helpers ---------- */
-
-    private static string? PromptForKey (IList<string> paths) {
-        var dlg = new OpenFileDialog {
-            Title = "Choose certificate",
-            Filter = "PKCS#12 (*.pfx)|*.pfx",
-            InitialDirectory = Path.GetDirectoryName(paths[0])
-        };
-        return dlg.ShowDialog() == true ? dlg.FileName : null;
+    private void Quit_Click (object sender, RoutedEventArgs e) {
+        this.Close();
     }
 }
