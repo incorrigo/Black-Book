5c5a06241d63380bccd1a395bbb12ba6c40365ec
incorrigo syx <westid@incorrigo.io>
2025-07-05 06:46:28 +0100

    Forensic profile deletion procedure reigns supreme

--- Black Book/Secure/ForensicProfileDeletion.cs
+++ Black Book/Secure/ForensicProfileDeletion.cs
@@ -1,6 +1,5 @@
 ﻿/// INCORRIGO SYX DIGITAL COMMUNICATION SYSTEMS
 // Forensic Profile Deletion
-// Securely deletes a profile in a forensic manner by corrupting its files and removing them
 using System;
 using System.IO;
 using System.Security.Cryptography;
@@ -10,69 +9,65 @@ namespace BlackBook.Security;
 
 public static class ForensicProfileDeletion {
 
-    /// <summary>
-    /// Deletes a profile by corrupting its files and removing them securely
-    /// </summary>
-    /// <param name="profileName"></param>
-    /// <param name="password"></param>
-    /// <param name="confirmDelete"></param>
-    /// <param name="usersRootDirectory"></param>
-    /// <param name="progress"></param>
-    /// <returns></returns>
-    /// <exception cref="InvalidOperationException"></exception>
-    /// <exception cref="DirectoryNotFoundException"></exception>
-    /// <exception cref="UnauthorizedAccessException"></exception>
+    // A process of removing a profile from the system that misrepresents the prospect of data recovery
     public static async Task DeleteProfileAsync (string profileName, string password, string confirmDelete, string usersRootDirectory, IProgress<double> progress) {
         if (confirmDelete != "DELETE")
-            throw new InvalidOperationException("Delete confirmation wasn't fully entered");
+            // This action is irreversible and permanent. Requires a firm commitment
+            throw new InvalidOperationException("Delete confirmation wasn't fully entered. If you want to delete a profile, you must make a firm commitment.");
         
         string profileDir = Path.Combine(usersRootDirectory, profileName);
         string[] targetFiles = { "file", "file.file" };
 
         // Check if the profile directory exists
         if (!Directory.Exists(profileDir))
-            throw new DirectoryNotFoundException("Profile directory doesn't exist");
+            throw new DirectoryNotFoundException("Profile directory doesn't exist. Forensic delete could not continue because the target directory doesn't exist");
 
         try {
-            // Validate the profile name and password
+            // Secure profile load makes successful login intrinsic to iniitate delete procedure
             await SecureProfileManager.LoadAsync(profileName, password, usersRootDirectory);
         }
         catch {
-            // A successful load indicates the password is correct
-            throw new UnauthorizedAccessException("Profile password validation failed");
+            // We do not proceed if the user could not be authenticated
+            throw new UnauthorizedAccessException("Profile password validation failed. Forensic delete could not continue because the secure profile login was not successful");
         }
 
-        // Deploy forensic data corruption
-        const int iterations = 58; // iterations of corruption
+        // Prepare a random binary matrix which will be applied
+        const int iterations = 58; // <-- this many times
         RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        // Calculate the total number of steps for progress reporting
+        int totalSteps = iterations * targetFiles.Length;
+        int currentStep = 0;
 
         foreach (var fileName in targetFiles) {
             var filePath = Path.Combine(profileDir, fileName);
             if (!File.Exists(filePath))
                 continue;
 
-            // Explicit forensic corruption
+            // Every iteration of the random binary matrix distorts the size of the file
             long originalLength = new FileInfo(filePath).Length;
 
-            // Initiate the corruption process using randomness
+            // Save every iteration of corruption to disk
             for (int i = 0; i < iterations; i++) {
                 int extraLength = Random.Shared.Next(256 * 1024, 1024 * 1024);
                 long newLength = originalLength + extraLength;
                 byte[] randomData = new byte[newLength];
                 rng.GetBytes(randomData);
                 await File.WriteAllBytesAsync(filePath, randomData);
-                progress.Report((double)(i + 1) / (iterations * targetFiles.Length));
+
+                // Progress bar update
+                currentStep++;
+                progress.Report((double)currentStep / totalSteps);
             }
         }
 
-        // False hope for the forensic investigator
+        // Create a new RSA key pair and an AES key for encryption
         using RSA rsa = RSA.Create(8192);
         using Aes aes = Aes.Create();
         aes.GenerateKey();
         aes.GenerateIV();
 
         foreach (var fileName in targetFiles) {
-            // Falsify hope for every file in profile directory
             var filePath = Path.Combine(profileDir, fileName);
             if (!File.Exists(filePath))
                 continue;
@@ -87,24 +82,26 @@ public static class ForensicProfileDeletion {
 
             byte[] encryptedKey = rsa.Encrypt(aes.Key, RSAEncryptionPadding.OaepSHA512);
 
-            // Save encrypted corrupted file
+            // Save what we've ended up with to disk
             await using var fs = File.Create(filePath);
             await fs.WriteAsync(encryptedKey);
             await fs.WriteAsync(aes.IV);
             await fs.WriteAsync(encryptedContent);
         }
 
-        // Shit for brains: rename and delete
         foreach (var fileName in targetFiles) {
             var filePath = Path.Combine(profileDir, fileName);
             if (File.Exists(filePath)) {
+                // Add "shit_for_brains" to the file name
                 var misleadName = Path.Combine(profileDir, fileName + "_shit_for_brains");
                 File.Move(filePath, misleadName, overwrite: true);
+
+                // Delete the file from disk
                 File.Delete(misleadName);
             }
         }
 
-        // Get rid of thde keys from memory
+        // Wipe the encryption keys from memory
         CryptographicOperations.ZeroMemory(aes.Key);
         CryptographicOperations.ZeroMemory(aes.IV);
         rsa.Clear();
