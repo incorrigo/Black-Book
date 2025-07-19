8e73f27a2e36cbd9310dc5a0983fb1fa47323fb7
incorrigo syx <westid@incorrigo.io>
2025-07-19 08:47:39 +0100

    objective listview update

--- Black Book/Views/ObjectiveManager.xaml.cs
+++ Black Book/Views/ObjectiveManager.xaml.cs
@@ -1,11 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using BlackBook.Models;
 using BlackBook.Storage;
 
 namespace BlackBook.Views;
 
 public partial class ObjectiveManager : UserControl {
+    private BlackBookContainer data;
+
     public ObjectiveManager () {
         InitializeComponent();
-        DataContext = SessionManager.Data!;
+        data = SessionManager.Data!;
+        DataContext = data;
+    }
+
+    private void ObjectivesList_MouseDoubleClick (object sender, MouseButtonEventArgs e) {
+        if (ObjectivesList.SelectedItem is Objective selectedObjective) {
+            var editWindow = new ObjectiveEntryWindow(selectedObjective) {
+                Owner = Window.GetWindow(this)
+            };
+
+            if (editWindow.ShowDialog() == true) {
+                ObjectivesList.Items.Refresh(); // Explicitly refresh UI list after editing
+            }
+        }
     }
 }
